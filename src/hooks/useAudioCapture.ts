5eeda7c497a9e044709cf5d2cf0facf05be7c5b1
Diego-DPL <info@diegodpl.com>
2024-10-04 10:22:25 +0200

    HTTPS y Despliegue

--- src/hooks/useAudioCapture.ts
+++ src/hooks/useAudioCapture.ts
@@ -59,7 +59,8 @@ const useAudioCapture = ({ setTranscriptions, selectedDeviceId }: UseAudioCaptur
       const combinedStreamLocal = destination.stream;
       setCombinedStream(combinedStreamLocal);
 
-      socketRef.current = new WebSocket('ws://localhost:8000/ws/audio');
+//      socketRef.current = new WebSocket('ws://localhost:8000/ws/audio');
+        socketRef.current = new WebSocket('wss://cleverthera-e0e22ef57185.herokuapp.com/ws/audio');
 
       socketRef.current.onopen = () => {
         console.log('Conectado al servidor de WebSocket');
