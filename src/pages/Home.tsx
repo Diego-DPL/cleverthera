be8487232a6ae1191696a29ef1286761befecd16
Diego DPL <info@diegodpl.com>
2024-10-24 14:09:30 +0200

    quitar warnings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-
 import { Button } from "../components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../components/ui/card"
-import { Mic, FileText, Brain, Clock, CheckCircle } from "lucide-react"
+import { Mic, FileText, Brain, CheckCircle } from "lucide-react"
 
 const Login: React.FC = () => {
 
