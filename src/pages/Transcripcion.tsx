f609716229835fdaa5d06ebc73bbaebbc1e91ed6
Diego DPL <info@diegodpl.com>
2024-12-30 18:02:09 +0100

    eliminando sin usar

--- src/pages/Transcripcion.tsx
+++ src/pages/Transcripcion.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "../components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select";
