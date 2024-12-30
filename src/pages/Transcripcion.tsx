edcba9850e45e2d361c4744ac8f642c3ca37fc98
Diego DPL <info@diegodpl.com>
2024-12-30 20:07:08 +0100

    errores fuera

--- src/pages/Transcripcion.tsx
+++ src/pages/Transcripcion.tsx
@@ -1,5 +1,5 @@
 // src/pages/TranscriptionPage.tsx
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "../components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select";
